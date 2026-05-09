e209d91a06532abfb4c04a917d114a2dfe8d08be
David Bonilla <thegrindnet@gmail.com>
2026-05-09 15:57:06 -0600

    Update base on vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/erick_garcia_website/",
   server: {
     port: 3001,
   },
